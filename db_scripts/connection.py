56ac9dc1c943caa4c86a9ed7bd864965243f2d61
sasapejcic <sasapejcic@gmail.com>
2019-11-15 09:24:42 +0100

    connection.py and create_db.py added.

---
+++ db_scripts/connection.py
@@ -0,0 +1,7 @@
+host="ec2-79-125-126-205.eu-west-1.compute.amazonaws.com"
+database="d9eqc1kq3rl4pl"
+user="hjarwfeiklyxyb"
+password="4b2862371520d9d6dcd0419778ed66ebdc426dbffa5d7cc9a42c44113b51d719"
+
+def connection():
+    return host, database, user, password
---
+++ db_scripts/create_db.py
@@ -0,0 +1,59 @@
+import os
+import psycopg2
+from connection import connection
+
+#DATABASE_URL = os.environ['DATABASE_URL']
+
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # read connection parameters
+        params = connection()
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(host=params[0],database=params[1], user=params[2], password=params[3])
+
+        # create a cursor
+        cur = conn.cursor()
+
+        # execute a statement
+        print('Droping existing tables...')
+        cur.execute('DROP TABLE IF EXISTS reviews, users, books;')
+
+        # Creating tables
+        print('Creating tables...')
+        cur.execute("CREATE TABLE IF NOT EXISTS books (isbn char(10) NOT NULL PRIMARY KEY, title varchar(100) NOT NULL, author varchar(40) NOT NULL, year integer NOT NULL);")
+
+       # close the communication with the PostgreSQL
+        cur.close()
+        print("Committing...")
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+connect()
+
+"""
+if __name__ == '__main__':
+    connect()
+
+try:
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cur = conn.cursor()
+    cur.execute("SELECT vendor_id, vendor_name FROM vendors ORDER BY vendor_name")
+    print("The number of parts: ", cur.rowcount)
+    row = cur.fetchone()
+
+    while row is not None:
+        print(row)
+        row = cur.fetchone()
+    cur.close()
+except(Exception, psycopg2.DatabaseError) as error:
+    print(error)
+"""
