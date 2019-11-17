902f8aab5ddc1b143b0a4098fa63a3121a640807
sasapejcic <sasapejcic@gmail.com>
2019-11-17 13:19:05 +0100

    "Config.py file added"

--- application.py
+++ application.py
@@ -5,7 +5,10 @@ from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+from flask_login import LoginManager
+
 app = Flask(__name__)
+app.config.from_object("config")
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -20,7 +23,15 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+# Check session
+login_manager = LoginManager()
+login_manager.init_app(app)
+is_auth=True
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", is_auth=is_auth)
